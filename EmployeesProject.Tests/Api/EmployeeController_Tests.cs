1832358b8bd9292d1151a2f5c0414a84723a6328
Daniel Méndez <36239306+danymendez@users.noreply.github.com>
2020-09-19 18:18:35 -0600

    se agregan pruebas unitarias

---
+++ EmployeesProject.Tests/Api/EmployeeController_Tests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using EmployeesProject.EL;
+using EmployeesProject.Tests.Utils;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace EmployeesProject.Tests.Api
+{
+   public class EmployeeController_Tests: IntegrationTest
+    {
+        private const string apiUrlEmployee = "/api/Employee";
+        public EmployeeController_Tests(ApiWebApplicationFactory fixture)
+          : base(fixture) { }
+
+        [Fact]
+        public async Task Insert_Should_Return_Employee()
+        {
+            var employee = new Employee
+            {
+                Id = 0,
+                Nombres = "Nombre uno",
+                Apellidos = "Nombre dos",
+                DUI = "00000",
+                FechaNacimiento = DateTime.Now,
+                ISSS=0,
+                NIT="",
+                Telefono=""
+            };
+            _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            var responseInsert = await _client.PostAsJsonAsync(apiUrlEmployee, employee);
+            var tupleInserted = await GetEntityResponse<Employee>(responseInsert);
+            Assert.Equal(HttpStatusCode.OK, tupleInserted.Item1);
+            Assert.NotNull(tupleInserted.Item2);
+
+
+        }
+
+        public async Task<Tuple<HttpStatusCode, T>> GetEntityResponse<T>(HttpResponseMessage response)
+        {
+            var forecast = JsonConvert.DeserializeObject<T>(
+              await response.Content.ReadAsStringAsync()
+            );
+            return new Tuple<HttpStatusCode, T>(response.StatusCode, forecast);
+        }
+    }
+}
---
+++ EmployeesProject.Tests/Utils/ApiWebApplicationFactory .cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using EmployeesProject.Api;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
+
+namespace EmployeesProject.Tests.Utils
+{
+    public class ApiWebApplicationFactory : WebApplicationFactory<Startup>
+    {
+
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            var projectDir = Directory.GetCurrentDirectory();
+            var configPath = Path.Combine(projectDir, "appsettings.json");
+            builder.ConfigureAppConfiguration((context, conf) =>
+            {
+
+                conf.AddJsonFile(configPath);
+
+            });
+
+        }
+    }
+}
